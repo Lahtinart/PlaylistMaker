7f638ae52e46bb6a0bc13fe9f74c42a50324e237
Lahtinart <lahtinart@gmail.com>
2025-07-08 19:29:41 +0200

    Home work 2
    Three buttons has been with interactive created.

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.playlistmaker
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -10,16 +11,27 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_poster)
 
-        val image = findViewById<ImageView>(R.id.poster)
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+            val button1: Button = findViewById(R.id.button1)
+            val button2: Button = findViewById(R.id.button2)
+            val button3: Button = findViewById(R.id.button3)
 
-        val imageClickListener = View.OnClickListener {
-            Toast.makeText(this@MainActivity, "Нажали на картинку!", Toast.LENGTH_SHORT).show()
+            button1.setOnClickListener(object : View.OnClickListener {
+                override fun onClick(v: View?) {
+                    Toast.makeText(this@MainActivity, "Нажата кнопка 1", Toast.LENGTH_SHORT).show()
+                }
+            })
+
+            button2.setOnClickListener {
+                Toast.makeText(this, "Нажата кнопка 2", Toast.LENGTH_SHORT).show()
+            }
+
+            button3.setOnClickListener {
+                Toast.makeText(this, "Нажата кнопка 3", Toast.LENGTH_SHORT).show()
+            }
         }
 
-        image.setOnClickListener(imageClickListener)
-    }
 }
