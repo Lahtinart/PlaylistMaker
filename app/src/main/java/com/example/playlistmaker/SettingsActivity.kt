9d192893804bb3b5a1a20b109919c92e82f4eaa7
Lahtinart <lahtinart@gmail.com>
2025-07-08 21:25:04 +0200

    Home Work 3a
    General dimensions have been added to dimens file and the "Back to Home Screen" button now work

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,13 +1,20 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
         setContentView(R.layout.activity_settings)
+
+        val backButton: TextView = findViewById(R.id.back_button)
+        backButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
