c03cf860340a943148b662273a051266100a8cad
Lahtinart <lahtinart@gmail.com>
2025-07-08 20:43:14 +0200

    Home work 3
    All activitis have been created and all buttons are now functional.

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -12,26 +13,28 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
 
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_main)
-            val button1: Button = findViewById(R.id.button1)
-            val button2: Button = findViewById(R.id.button2)
-            val button3: Button = findViewById(R.id.button3)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val button1: Button = findViewById(R.id.button1)
+        val button2: Button = findViewById(R.id.button2)
+        val button3: Button = findViewById(R.id.button3)
 
-            button1.setOnClickListener(object : View.OnClickListener {
-                override fun onClick(v: View?) {
-                    Toast.makeText(this@MainActivity, "Нажата кнопка 1", Toast.LENGTH_SHORT).show()
-                }
-            })
-
-            button2.setOnClickListener {
-                Toast.makeText(this, "Нажата кнопка 2", Toast.LENGTH_SHORT).show()
-            }
-
-            button3.setOnClickListener {
-                Toast.makeText(this, "Нажата кнопка 3", Toast.LENGTH_SHORT).show()
-            }
+        button1.setOnClickListener {
+            val intent = Intent(this, SearchActivity::class.java)
+            startActivity(intent)
         }
 
+        button2.setOnClickListener {
+            val intent = Intent(this, MediaLibraryActivity::class.java)
+            startActivity(intent)
+        }
+
+        button3.setOnClickListener {
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
+        }
+    }
 }
+
+
