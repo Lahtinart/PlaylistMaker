80ed59bede72da31d890d5c783d757700022910e
Lahtinart <lahtinart@gmail.com>
2025-07-09 16:59:30 +0200

    Correction 1
    added Switch on/off and SettigsAktivity has onlz final()

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -2,8 +2,11 @@ package com.example.playlistmaker
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.switchmaterial.SwitchMaterial
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,9 +15,19 @@ class SettingsActivity : AppCompatActivity() {
 
         val backButton: TextView = findViewById(R.id.back_button)
         backButton.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+
+
             finish()
         }
+        val mySwitch = findViewById<SwitchMaterial>(R.id.my_switch)
+
+        mySwitch.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                Log.d("Switcher", "Включено")
+            } else {
+                Log.d("Switcher", "Выключено")
+            }
+        }
     }
+
 }
