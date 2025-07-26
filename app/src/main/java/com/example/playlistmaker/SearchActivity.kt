4160f8ac4172dfff6da85a9fb6eee013616d55de
Lahtinart <lahtinart@gmail.com>
2025-07-26 12:39:08 +0200

    Howework 6.1

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,12 +1,56 @@
 package com.example.playlistmaker
 
+import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import com.google.android.material.appbar.MaterialToolbar
 
 class SearchActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
 
+    private lateinit var searchEditText: EditText
+    private lateinit var clearButton: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        val sharedPrefs = getSharedPreferences("settings", Context.MODE_PRIVATE)
+        val isDarkMode = sharedPrefs.getBoolean("dark_theme", false)
+
+        AppCompatDelegate.setDefaultNightMode(
+            if (isDarkMode) AppCompatDelegate.MODE_NIGHT_YES
+            else AppCompatDelegate.MODE_NIGHT_NO
+        )
+
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val toolbar = findViewById<MaterialToolbar>(R.id.topAppBar)
+        toolbar.setNavigationOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+            finish()
+        }
+
+        searchEditText = findViewById(R.id.search_edit_text)
+        clearButton = findViewById(R.id.clear_button)
+
+        clearButton.setOnClickListener {
+            searchEditText.setText("")
+        }
+
+        searchEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearButton.visibility = if (s.isNullOrEmpty()) View.GONE else View.VISIBLE
+                // TODO: Здесь будет логика поиска
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
     }
 }
